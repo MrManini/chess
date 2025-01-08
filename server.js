89460a258084f1bd47293e5f1c233267688e8df4
kevin <kevintorregrosa2304@gmail.com>
2025-01-07 23:38:21 -0500

    added https

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
 app.use(express.static('public'));
 
@@ -7,6 +10,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(80, () => {
-    console.log('Server running on http://localhost/');
+https.createServer({
+    key: fs.readFileSync(`/etc/letsencrypt/live/chess.servegame.com/privkey.pem`),
+    cert: fs.readFileSync(`/etc/letsencrypt/live/chess.servegame.com/fullchain.pem`)
+}, app).listen(443, () => {
+    
+    console.log(`HTTPS Server running on https://localhost:443`);
+});
+
+http.createServer((req, res) => {
+    res.writeHead(301, { "Location": `https://${req.headers.host}${req.url}` });
+    res.end();
+}).listen(80, () => {
+    console.log(`HTTP server redirecting to HTTPS on port 80`);
 });
