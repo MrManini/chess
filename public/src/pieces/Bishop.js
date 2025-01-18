010099a5080391f9f3114663989c2e89918c352a
kevin <kevintorregrosa2304@gmail.com>
2025-01-18 18:38:58 -0500

    fixed named imports

--- public/src/pieces/Bishop.js
+++ public/src/pieces/Bishop.js
@@ -1,4 +1,4 @@
-import Piece from './Piece.js';
+import { Piece } from './Piece.js';
 
 export class Bishop extends Piece {
     constructor(color, square) {
--- public/src/pieces/Rook.js
+++ public/src/pieces/Rook.js
@@ -1,4 +1,4 @@
-import Piece from './Piece.js';
+import { Piece } from './Piece.js';
 
 export class Rook extends Piece {
     constructor(color, square) {
