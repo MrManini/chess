d69584eaeff15cf04cf1721abafd9d4bad1e2194
kevin <kevintorregrosa2304@gmail.com>
2025-01-18 18:29:29 -0500

    added path to requirements (how was this working before without this??)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 app.use(express.static('public'));
 
